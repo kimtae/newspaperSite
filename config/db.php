5a76d4f348b874fc515d3008553d11c76cba8f07
lylkl <25lykl13@gmail.com>
2021-07-29 17:57:18 +0800

    +db.php // KEKL

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=newspapersite',
+    'dsn' => 'mysql:host=localhost;dbname=newspaper',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
