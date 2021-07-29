95c085f854ab53c84ac2f38126e12e1268bbd474
kduvs <vernitemnemoi2007@bk.ru>
2021-07-29 17:50:54 +0800

    проверка

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=cumass',
+    'dsn' => 'mysql:host=localhost;dbname=newspapersite',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
